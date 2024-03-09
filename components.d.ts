6074f61bbcb512cf65d53d78ee90a6bf6d36a00b
suli <suli249221@163.com>
2024-03-09 16:26:11 +0800

    除了除还剩图片懒加载和校验和一些小细节

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
     GoodsPagination: typeof import('./src/components/GoodsPagination.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
